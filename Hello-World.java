6e6bec5dc8f7e98e821dcca7448cb9ee58a14a69
MadhavKapila <57823522+MadhavKapila@users.noreply.github.com>
2019-12-08 16:45:13 +0530

    Add files via upload
    JAVA Hello-World Pogram

---
+++ Hello-World.java
@@ -0,0 +1,15 @@
+import java.util.*;  // importing package for input
+class hello-world   //class declaration
+{
+public static void main()  //main() method declaration
+{
+int l,n;
+Scanner in=new Scaner(System.in);  //creating Scaner class object
+System.out.println("Enter no. of times you want to print HELLO-WORLD message");
+n=in.nextInt();
+for(l=1;l<=n;l++)
+{
+System.out.println("Welcome to Java Programming language!!! Hello-World program");
+}
+}
+}
