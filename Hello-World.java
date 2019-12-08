2e1a730be980f84b84b1215fc99212de4e449fc7
MadhavKapila <57823522+MadhavKapila@users.noreply.github.com>
2019-12-08 16:50:19 +0530

    Update Hello-World.java
    Changed for loop to while loop

--- Hello-World.java
+++ Hello-World.java
@@ -3,13 +3,14 @@ class hello-world   //class declaration
 {
 public static void main()  //main() method declaration
 {
-int l,n;
+int l,n;l=1;
 Scanner in=new Scaner(System.in);  //creating Scaner class object
 System.out.println("Enter no. of times you want to print HELLO-WORLD message");
 n=in.nextInt();
-for(l=1;l<=n;l++)
+while(l<=n)
 {
 System.out.println("Welcome to Java Programming language!!! Hello-World program");
+l++;
 }
 }
 }
